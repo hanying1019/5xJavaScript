359a75d9eee440e83a352d74fd4a45a9998b7d14
HanYing Liu <nina19971019@gmail.com>
2021-04-19 11:32:04 +0800

    no message

--- tttt.js
+++ tttt.js
@@ -1,7 +1,40 @@
-let hero = {
-  name: '醫護',
-  power:5,
-  healling: function(){
-  },
+// let hero = {
+//   name: '醫護',
+//   power:5,
+//   healling: function(){
+//   },
   
+// }
+
+var hero = {
+  name: '悟空',
+  sayMyName: function() {
+    console.log(this.name);
+  }
+};
+
+hero.sayMyName();   // A
+
+var speakOut = hero.sayMyName;
+speakOut();         // B
+
+const someone = { name: '路人' }
+
+hero.sayMyName.call(someone);  // C
+
+function here() {
+  console.log(this);
 }
+
+const there = () => {
+  console.log(this);
+}
+
+here();   // D
+there();  // E
+
+- A：悟空
+- B：undefined
+- C：路人
+- D：全域物件
+- E：全域物件
